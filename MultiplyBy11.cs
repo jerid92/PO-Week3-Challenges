f373cef1e743bb73e09b4841a7b7cdc2ec24359c
jerid92 <33670855+jerid92@users.noreply.github.com>
2022-02-03 13:33:52 -0600

    Add files via upload

---
+++ MultiplyBy11.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace Times11
+{
+
+	public class MultiplyBy11
+	{
+		static void Main(string[] args)
+		{
+			string eleven1 = "11";
+			string eleven2 = "111111111";
+			string eleven3 = "1213200020";
+			string eleven4 = "1217197941";
+			string eleven5 = "9473745364784876253253263723";
+
+			Console.WriteLine(MultiBy11(eleven1));
+			Console.WriteLine(MultiBy11(eleven2));
+			Console.WriteLine(MultiBy11(eleven3));
+			Console.WriteLine(MultiBy11(eleven4));
+			Console.WriteLine(MultiBy11(eleven5));
+
+		}
+
+		public string MultiBy11(string result)
+        {
+			int length = result.Length + 1;
+			StringBuilder product = new StringBuilder();
+			int remainder = 0;
+			product.Append(result[result.Length - 1]);
+			for(int i = 0; i < result.Length - 1; i++)
+            {
+				int number = (int)Char.GetNumericValue(result[i]) + (int)Char.GetNumericValue(result[i - 1]) + remainder;
+				int digit = number % 10;
+				result.Insert(0, digit);
+				if (number > 10 && digit != 0 || number == 10)
+                {
+					remainder = 1;
+                }
+				else
+                {
+					remainder = 0;
+                }
+				if (i == 1 && number > 10)
+                {
+					result.Insert(0, (int)Char.GetNumericValue(result[0]) + 1);
+                }
+				else if (i == 1)
+                {
+					result.Insert(0, (int)Char.GetNumericValue(result[0]));
+                }
+            }
+
+			return result.ToString();
+        }
+	}
+}
---
+++ NoStringInArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace NoStringsInArray
+{
+    class NoStringInArray
+    {
+        static void Main(string[] args)
+        {
+            NoStringInArray array = new NoStringInArray();
+
+            ArrayList list1 = new ArrayList() { 1, 2, "a", "b" };
+            ArrayList list2 = new ArrayList() { 1, "a", "b", 0, 15 };
+            ArrayList list3 = new ArrayList() { 1, 2, "aasf", "1", "123", 123 };
+            ArrayList outcome = new ArrayList();
+
+            outcome = array.ArrayFiltered(list1);
+            array.viewOutcome(outcome);
+
+            outcome = array.ArrayFiltered(list2);
+            array.viewOutcome(outcome);
+
+            outcome = array.ArrayFiltered(list3);
+            array.viewOutcome(outcome);
+
+
+        }
+
+        public ArrayList ArrayFiltered(ArrayList arl)
+        {
+            ArrayList outcome = new ArrayList();
+
+            foreach(var item in arl)
+            {
+                if(item.GetType() == typeof(int))
+                {
+                    outcome.Add(item);
+                }
+            }
+
+            return outcome;
+        }
+
+        public void viewOutcome(ArrayList arl)
+        {
+            ArrayList outcome = new ArrayList();
+            outcome = ArrayFiltered(arl);
+            string st = "[ ";
+            for (int i = 0; i <= outcome.Count; i++)
+            {
+                if( i == outcome.Count - 1)
+                {
+                    st += $"{outcome[i]} ]";
+                }
+                else
+                {
+                    st += $"{outcome[i]}, ";
+                }
+            }
+            Console.WriteLine(st);
+        }
+    }
+}
